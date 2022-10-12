105c868cd680c273bdcdb464f38886c5189e73fe
Amarnath Barpanda <47183341+amarnathbarpanda@users.noreply.github.com>
2022-10-12 13:54:41 +0530

    add user router

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ const homeController = require('../controllers/home_controller');
 const router = express.Router();
 console.log('Router loaded');
 
+// accessing users routes , for further routes, access from here
+// router.use('/routerName', require('./routerFile'));
+
+router.use('/users', require('./users'));
+
 router.get('/', homeController.home);
 
 module.exports = router;
